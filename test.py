5a76bcce25c2710cb9dd3b1fe42bced848960775
24parida <24parida@elmbrookstudents.org>
2022-07-10 13:18:35 -0500

    scrapping entire game in favor of pygame
    no other choice :/

--- test.py
+++ test.py
@@ -4,6 +4,8 @@
 import snake
 from tkinter import *
 import random
+import neat
+import os
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
@@ -18,38 +20,84 @@ global gen_count
 gen_count = 1
 
 # gen - snake - properties
-global snakes, foods, directions, lives, moves, scores
-snakes = []
-foods = []
-directions = []
-lives = []
-moves = []
-scores = []
 
 
-def initialize():
-    print("initializing")
-    for i in range(gen_count):
-        snakes.append(Snake(i))
-        foods.append(Food(i))
-        directions.append('down')
-        lives.append(True)
-        moves.append(30)
-        scores.append(0)
+# NEAT implementation
+global nets, ge
 
 
-def restart():
-    for i in range(gen_count):
-        snakes[i] = Snake(i)
-        foods[i] = Food(i)
-        directions[i] = 'down'
-        lives[i] = True
-        moves[i] = 30
-        scores[i] = 0
-    label.config(text="Score:{}".format(scores[0]))
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation, config_path)
+
+    p = neat.Population(config)
+
+    # p.add_reporter(neat.StdOutReporter(True))
+    # stats = neat.StatisticsReporter()
+    # p.add_reporter(stats)
+
+    winner = p.run(fitness, 5)
+
+    # print('\nBest genome:\n{!s}'.format(winner))
 
 
-def snakes_still_alive():
+def fitness(genomes, config):
+    global nets, ge
+    nets = []
+    ge = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        g.fitness = 0
+        ge.append(g)
+
+    # for i in range(gen_count):
+    #     next_turn(i)
+
+    # # window.after(SPEED, fitness, genomes, config)
+    # window.after(SPEED, next_turns)
+
+
+def get_action(num):
+    global nets
+
+    # output = nets[num].activate(state_of_game(num))
+    output = nets[num].activate((0, 1))
+
+    return output
+
+
+# def initialize():
+#
+#     print("initializing")
+#     snakes.clear()
+#     foods.clear()
+#     lives.clear()
+#     moves.clear()
+#     scores.clear()
+#
+#     for i in range(gen_count):
+#         snakes.append(Snake(i))
+#         foods.append(Food(i))
+#         directions.append('down')
+#         lives.append(True)
+#         moves.append(30)
+#         scores.append(0)
+#
+#
+# def restart():
+#     for i in range(gen_count):
+#         snakes[i] = Snake(i)
+#         foods[i] = Food(i)
+#         directions[i] = 'down'
+#         lives[i] = True
+#         moves[i] = 30
+#         scores[i] = 0
+#     # label.config(text="Score:{}".format(scores[0]))
+#
+#
+def snakes_still_alive(lives):
     for i in range(gen_count):
         if lives[i]:
             return True
@@ -89,105 +137,168 @@ class Food:
 
 
 def next_turns():
-    for i in range(gen_count):
-        next_turn(i)
+    print("conducting next turn")
+    # for i in range(gen_count):
+    #     next_turn(i)
+    window.after(SPEED, next_turns)
 
 
-def next_turn(num):
-    if moves[num] == 0:
-        lives[num] = False
-        game_over(num)
+def eval_genomes(genomes, config):
+    snakes = []
+    foods = []
+    directions = []
+    lives = []
+    moves = []
 
-    if lives[num]:
-        moves[num] -= 1
-        x, y = snakes[num].coordinates[0]
+    nets = []
+    ge = []
 
-        if directions[num] == "up":
-            y -= SPACE_SIZE
-        elif directions[num] == "down":
-            y += SPACE_SIZE
-        elif directions[num] == "left":
-            x -= SPACE_SIZE
-        elif directions[num] == "right":
-            x += SPACE_SIZE
+    # for _, g in genomes:
+    for _ in range(genomes):
+        # net = neat.nn.FeedForwardNetwork.create(g, config)
+        # nets.append(net)
+        # g.fitness = 0
+        # ge.append(g)
+        snakes.append(Snake(_))
+        foods.append(Food(_))
+        directions.append('down')
+        lives.append(True)
+        moves.append(30)
 
-        snakes[num].coordinates.insert(0, (x, y))
+    print("conducting next turn")
 
-        square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake"+str(num))
-        snakes[num].squares.insert(0, square)
+    for a in range(len(moves)):
+        if moves[a] == 0:
+            lives[a] = False
+            lives[a] = False
+            snakes[a].restart()
+            foods[a].restart()
+            # nets.pop(num)
+            # ge.pop(num)
+            canvas.delete("snake" + str(a))
+            canvas.delete("food" + str(a))
 
-        found_food = False
-        for a, b in snakes[num].coordinates:
-            if a == foods[num].coordinates[0] and b == foods[num].coordinates[1]:
+    run = True
+    while run and snakes_still_alive(lives):
+        for num in range(len(snakes)):
+            if lives[num]:
+                moves[num] -= 1
+                x, y = snakes[num].coordinates[0]
 
-                found_food = True
-                scores[num] += 1
-                moves[num] += 20
+                if directions[num] == "up":
+                    y -= SPACE_SIZE
+                elif directions[num] == "down":
+                    y += SPACE_SIZE
+                elif directions[num] == "left":
+                    x -= SPACE_SIZE
+                elif directions[num] == "right":
+                    x += SPACE_SIZE
 
-                label.config(text="Score:{}".format(scores[0]))
+                snakes[num].coordinates.insert(0, (x, y))
 
-                canvas.delete("food"+str(num))
-                foods[num] = Food(num)
+                square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake"+str(num))
+                snakes[num].squares.insert(0, square)
 
-        if not found_food:
-            del snakes[num].coordinates[-1]
+                found_food = False
+                for a, b in snakes[num].coordinates:
+                    if a == foods[num].coordinates[0] and b == foods[num].coordinates[1]:
 
-            canvas.delete(snakes[num].squares[-1])
+                        found_food = True
+                        # ge[num].fitness += 5
+                        moves[num] += 20
 
-            del snakes[num].squares[-1]
+                        canvas.delete("food"+str(num))
+                        foods[num] = Food(num)
 
-        # AI BRAIN
+                if not found_food:
+                    del snakes[num].coordinates[-1]
 
-        if check_collisions(snakes[num]):
-            game_over(num)
-        else:
-            state_of_game()
+                    canvas.delete(snakes[num].squares[-1])
 
-        window.after(SPEED, next_turn, num)
-    elif not snakes_still_alive():
-        print("restarting")
-        restart()
-        for i in range(gen_count):
-            window.after(SPEED, next_turn, i)
+                    del snakes[num].squares[-1]
+
+                # AI BRAIN
+
+                if check_collisions(snakes[num]):
+                    # print("direction fucked up: " + str(directions[num]))
+                    lives[num] = False
+                    snakes[num].restart()
+                    foods[num].restart()
+                    canvas.delete("snake" + str(num))
+                    canvas.delete("food" + str(num))
+
+                else:
+                    # outputs = get_action(num)
+                    outputs = [0, 1, 0, 0]
+                    h_val = 0
+                    h_ind = 0
+                    for x, i in enumerate(outputs):
+                        if i > h_val:
+                            h_ind = x
+                            h_val = i
+
+                    if h_ind == 0:
+                        directions[num] = change_direction("up", num, directions[num])
+                        print("moved up")
+
+                    elif h_ind == 1:
+                        directions[num] = change_direction("down", num, directions[num])
+                        print("moved down")
+
+                    elif h_ind == 2:
+                        directions[num] = change_direction("right", num, directions[num])
+                        print("moved right")
+
+                    elif h_ind == 3:
+                        directions[num] = change_direction("left", num, directions[num])
+                        print("moved left")
+            window.after(SPEED)
 
 
-def change_direction(new_direction, num):
+
+def change_direction(new_direction, num, current_dir):
+    dir = ''
     if new_direction == 'left':
-        if directions[num] != 'right':
-            directions[num] = new_direction
+        if current_dir != 'right':
+            dir = new_direction
     elif new_direction == 'right':
-        if directions[num] != 'left':
-            directions[num] = new_direction
+        if current_dir != 'left':
+            dir = new_direction
     elif new_direction == 'up':
-        if directions[num] != 'down':
-            directions[num] = new_direction
+        if current_dir != 'down':
+            dir = new_direction
     elif new_direction == 'down':
-        if directions[num] != 'up':
-            directions[num] = new_direction
+        if current_dir != 'up':
+            dir = new_direction
+    return dir
 
 
 def check_collisions(snake):
     x, y = snake.coordinates[0]
 
     if x < 0 or x >= GAME_WIDTH:
+        print("hit x")
         return True
     elif y < 0 or y >= GAME_HEIGHT:
+        print("hit y")
         return True
 
     for body_part in snake.coordinates[1:]:
         if x == body_part[0] and y == body_part[1]:
-            print('hitting body working')
+            print("hit body")
             return True
 
     return False
 
 
-def game_over(num):
-    lives[num] = False
-    snakes[num].restart()
-    foods[num].restart()
-    canvas.delete("snake"+str(num))
-    canvas.delete("food"+str(num))
+# def game_over(num):
+#     lives[num] = False
+#     snakes[num].restart()
+#     foods[num].restart()
+#     # nets.pop(num)
+#     # ge.pop(num)
+#     canvas.delete("snake"+str(num))
+#     canvas.delete("food"+str(num))
 
 
 def state_of_game(num):
@@ -232,10 +343,9 @@ window.resizable(False, False)
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
-initialize()
 
-label = Label(window, text="Score:{}".format(scores[0]), font=('consolas', 40))
-label.pack()
+# label = Label(window, text="Score:{}".format(scores[0]), font=('consolas', 40))
+# label.pack()
 window.update()
 
 window_width = window.winfo_width()
@@ -253,5 +363,10 @@ window.bind('<Right>', lambda event: change_direction('right', 0))
 window.bind('<Up>', lambda event: change_direction('up', 0))
 window.bind('<Down>', lambda event: change_direction('down', 0))
 
-next_turns()
+local_dir = os.path.dirname(__file__)
+config_path = os.path.join(local_dir, 'config-feedforward.txt')
+# run(config_path)
+# initialize()
+genomes = 1
+eval_genomes(genomes, config_path)
 window.mainloop()
